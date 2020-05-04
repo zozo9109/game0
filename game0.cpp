0c3d4d66d58494f07d31e0007df987d094fff12b
zozo9109@naver.com <zozo9109@naver.com>
2020-05-04 15:56:10 +0900

    변경

--- game0.cpp
+++ game0.cpp
@@ -1,20 +1,170 @@
-﻿// game0.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
-//
+﻿#include <stdio.h>
+#include <bangtal.h>
+#include <time.h>
+#include <stdlib.h>
 
-#include <iostream>
+SceneID scene1;
+TimerID timer1;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+ObjectID A, B, C, D, E, F, G, H, I, J, K, L, M, N, O;
+
+
+void character() {
+
+
+    scene1 = createScene("교실", "classroom.jpg");
+
+    A = createObject("teacher1.png");
+    locateObject(A, scene1, 900, 130);
+    scaleObject(A, 0.5f);
+    showObject(A);
+
+
+    B = createObject("student1.png");
+    locateObject(B, scene1, 100, 0);
+    scaleObject(B, 0.8f);
+    showObject(B);
+
+    C = createObject("student2.png");
+    locateObject(C, scene1, 600, 0);
+    scaleObject(C, 0.8f);
+    showObject(C);
+
+    D = createObject("student3.png");
+    locateObject(D, scene1, 1100, 0);
+    scaleObject(D, 0.8f);
+    showObject(D);
+
+
+    E = createObject("sleepingstudent2.png");
+    locateObject(E, scene1, 600, 0);
+    hideObject(E);
+
+
+    F = createObject("teacher2.png");
+    locateObject(F, scene1, 900, 130);
+    scaleObject(F, 0.5f);
+    hideObject(F);
+
+
+    
+    H = createObject("play.png");
+    locateObject(H, scene1, 640, 100);
+    hideObject(H);
+
+    I = createObject("end.png");
+    locateObject(I, scene1, 640, 100);
+    showObject(I);
+
+    J = createObject("start.png");
+    locateObject(J, scene1, 640, 150);
+    showObject(J);
+
+    K = createObject("sleepButton.png");
+    locateObject(K, scene1, 350, 0);
+    scaleObject(K, 0.5f);
+    hideObject(K);
+
+
+    L = createObject("getupButton.png");
+    locateObject(L, scene1, 850, 0);
+    scaleObject(L, 0.5f);
+    hideObject(L);
+
+
+    M = createObject("speechbubble1.png");
+    locateObject(M, scene1, 800, 150);
+    scaleObject(M, 0.8f);
+    hideObject(M);
+
+
+    N = createObject("speechbubble2.png");
+    locateObject(N, scene1, 800, 150);
+    scaleObject(N, 0.8f);
+    hideObject(N);
+
+
+    O = createObject("speechbubble3.png");
+    locateObject(O, scene1, 800, 150);
+    scaleObject(O, 0.8f);
+    hideObject(O);
+
+
 }
 
-// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
-// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
 
-// 시작을 위한 팁: 
-//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
-//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
-//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
-//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
-//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
-//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
+
+void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
+    //endButton버튼 누르면 게임종류
+    if (object == I) {
+        endGame();
+    }
+    //스타트 버튼을 누르면 endButton 사라지고 플레이 버튼 생긴다
+    else if (object == J) {
+        hideObject(I);
+        hideObject(J);
+        showObject(H);
+    }
+    //플레이 버튼을 누르면 교수가 교수1에세 교수 2 교수 3을 반복적으로 한다. 여기서 학생을 클릭시 학생이 자다 안자다가 한다. 타이머 작동된다 또 sleep버튼이랑 getup 버튼이 생김
+    // 조건에 따라 hide했다 slow 했다 하기로 a가 하이드면 b가 쇼 하는 거로
+    else if (object == H) {
+        startTimer(timer1);
+        hideObject(H);
+        showObject(K);
+        showObject(L);
+        //teacher1이 teacher2로 바뀜
+        hideObject(A);
+        showObject(F);
+
+
+
+
+       
+    }
+
+
+    //sleep Button을 클릭시 student2 잠자고 get up을 클릭시 일어나기
+    else if (object == K) {
+        hideObject(C);
+        showObject(E);
+        showObject(N);
+        //여기서 teacher3이랑 겹쳐질시 하트 깎임
+
+    }
+
+    //getupButton 클릭시 일어나기
+    else if (object == L) {
+        hideObject(E);
+        showObject(C);
+        showObject(M);
+
+    }
+    //그냥 사람 클릭해도 자다가 안자다가 변함
+    else if (object == C) {
+        hideObject(C);
+        showObject(E);
+        showObject(N);
+    }
+    else if (object == E) {
+        hideObject(E);
+        showObject(C);
+        showObject(M);
+    }
+    
+
+
+}
+
+
+
+
+int main() {
+
+    setMouseCallback(mouseCallback);
+    scene1 = createScene("교실", "classroom.jpg");
+    character();
+    startGame(scene1);
+
+
+
+}
